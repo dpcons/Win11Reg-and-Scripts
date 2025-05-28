0a7bdfe15b196030f77861106e78b4e1ffbfa48b
Marco Dal Pino <info@dpcons.com>
2025-05-28 13:51:56 +0200

    Added Service Description

--- Script/CreateServiceListCSV.ps1
+++ Script/CreateServiceListCSV.ps1
@@ -2,11 +2,16 @@ Get-Service | ForEach-Object {
     $serviceName = $_.Name
     $status = $_.Status
     $startType = (Get-CimInstance -ClassName Win32_Service -Filter "Name='$serviceName'").StartMode
+    $description = (Get-CimInstance -ClassName Win32_Service -Filter "Name='$serviceName'").Description
+    if (-not $description) {
+        $description = "No description available"
+    }
 
     [PSCustomObject]@{
         Service   = $serviceName
         StartType = $startType
         Status    = $status
+        Description = $description
     }
 } | Export-Csv -Path .\ServiceList.csv -NoTypeInformation -Encoding UTF8
 
