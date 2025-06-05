65bf9a33a690b72fc9e005d56e375501b282792d
Marco Dal Pino <info@dpcons.com>
2025-06-05 10:26:54 +0200

    Write Log FIle only for changes done, not for NOOP

--- Script/UpdateCSV-ImportXML.ps1
+++ Script/UpdateCSV-ImportXML.ps1
@@ -2,13 +2,10 @@ $logFile = ".\UpdateServices.log"
 
 function Write-Log {
     param (
-        [string]$Message,
-        [string]$Color = "White"
+        [string]$Message
     )
-    # Scrivi su file
+    # Scrivi solo su file
     $Message | Out-File -FilePath $logFile -Append -Encoding UTF8
-    # Scrivi a video
-    Write-Host $Message -ForegroundColor $Color
 }
 
 Import-Csv -Path .\ServiceList.csv | ForEach-Object {
@@ -16,22 +13,22 @@ Import-Csv -Path .\ServiceList.csv | ForEach-Object {
     $service = Get-Service -Name $_.Service -ErrorAction SilentlyContinue
 
     if (-not $service) {
-        Write-Log "Service $($_.Service) not found." "Red"
+        Write-Host "Service $($_.Service) not found." -ForegroundColor Red
         return
     }
 
     if ($service.StartType -ne $_.StartType) {
-        Write-Log "Changing StartType for service $($service.Name)" "Yellow"
+        Write-Host "Changing StartType for service $($service.Name)" -ForegroundColor Yellow
         try {
             $service | Set-Service -StartupType $_.StartType -ErrorAction Stop
             $changed = $true
         } catch {
-            Write-Log "Error changing StartType for $($service.Name): $_" "Red"
+            Write-Host "Error changing StartType for $($service.Name): $_" -ForegroundColor Red
         }
     }
 
     if ($service.Status -ne $_.Status) {
-        Write-Log "Changing Status for service $($service.Name)" "Yellow"
+        Write-Host "Changing Status for service $($service.Name)" -ForegroundColor Yellow
         try {
             if ($_.Status -eq "Running") {
                 Start-Service -Name $service.Name -ErrorAction Stop
@@ -40,15 +37,17 @@ Import-Csv -Path .\ServiceList.csv | ForEach-Object {
             }
             $changed = $true
         } catch {
-            Write-Log "Error changing Status for $($service.Name): $_" "Red"
+            Write-Host "Error changing Status for $($service.Name): $_" -ForegroundColor Red
         }
     }
 
     if ($changed) { 
         $service = Get-Service -Name $_.Service 
-        Write-Log "Service: $($service.Name) - StartType: $($service.StartType) - Status: $($service.Status) updated." "Green"
+        $logMsg = "Service: $($service.Name) - StartType: $($service.StartType) - Status: $($service.Status) updated."
+        Write-Host $logMsg -ForegroundColor Green
+        Write-Log $logMsg
     } else {
-        Write-Log "No changes for service $($service.Name)." "Cyan"
+        Write-Host "No changes for service $($service.Name)." -ForegroundColor Cyan
     }
 }
 
