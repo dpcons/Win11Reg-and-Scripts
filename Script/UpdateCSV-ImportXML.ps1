ffe33e93e4f2a511f18f6e5dd4e76bf7bcb2737f
Marco Dal Pino <info@dpcons.com>
2025-10-09 13:54:51 +0200

    CleanUP Repository and disable Applocker funzion in services Script

--- Script/UpdateCSV-ImportXML.ps1
+++ Script/UpdateCSV-ImportXML.ps1
@@ -73,14 +73,14 @@ Import-Csv -Path .\ServiceList.csv | ForEach-Object {
     }
 }
 
-Write-Host "Stop AppLock service" -ForegroundColor Red
+# Write-Host "Stop AppLock service" -ForegroundColor Red
 
-AppIdTel.exe Stop
+# AppIdTel.exe Stop
 # Write-Host "Clean AppLock cache" -ForegroundColor yellow
 # .\pulizia_cache_applocker.ps1
-Write-Host "Set AppLock Policy" -ForegroundColor yellow
-Set-AppLockerPolicy -XMLPolicy Applock-Policy.XML
-Get-AppLockerPolicy -Local -Xml > AppLock-Status.XML
-Write-Host "AppLock Policy set successfully. Look at AppLock-Status.XML for details." -ForegroundColor Green
-Write-Host "Start AppLock service" -ForegroundColor Green
-AppIdTel.exe Start
+# Write-Host "Set AppLock Policy" -ForegroundColor yellow
+# Set-AppLockerPolicy -XMLPolicy Applock-Policy.XML
+# Get-AppLockerPolicy -Local -Xml > AppLock-Status.XML
+# Write-Host "AppLock Policy set successfully. Look at AppLock-Status.XML for details." -ForegroundColor Green
+# Write-Host "Start AppLock service" -ForegroundColor Green
+# AppIdTel.exe Start
